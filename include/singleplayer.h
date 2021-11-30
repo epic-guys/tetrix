4b4e9ede91e23f8df7931689ca5c86c5518927b8
Alvise Favero <888851@stud.unive.it>
2021-11-30 12:39:54 +0100

    Ripristinate alcune funzioni per tetramini

---
+++ include/singleplayer.h
@@ -0,0 +1,6 @@
+#ifndef _SINGLEPLAYER_H_
+#define _SINGLEPLAYER_H_
+
+void newGameSingle();
+
+#endif
--- src/tetrimino.c
+++ src/tetrimino.c
@@ -26,7 +26,7 @@
  * ██████
  *   ██
  */
-enum TetriminoType
+typedef enum TetriminoType
 {
     T_I,
     T_J,
@@ -35,9 +35,10 @@ enum TetriminoType
     T_O,
     T_Z,
     T_T
-}
+} TetriminoType;
 
-const allTypes[7] = {T_I,T_J,T_L,T_O,T_S,T_T,T_Z};
+/* In caso serva fare un loop di tutti i tipi */
+const TetriminoType ALL_T_TYPES[7] = { T_I, T_J, T_L, T_O, T_S, T_T, T_Z };
 
 /**
  * @brief Non è un typo, in inglese
@@ -56,26 +57,100 @@ const allTypes[7] = {T_I,T_J,T_L,T_O,T_S,T_T,T_Z};
  */
 typedef struct Tetrimino
 {
-    int* values;
-    int cols;
-    int rows;
-    enum TetriminoType type;
+    const int* values;
+    const int cols;
+    const int rows;
+    const TetriminoType type;
 } Tetrimino;
 
-/**
- * Per gestire la rotazione si potrebbe
- * usare un int, ma con l'enum si limitano
- * le rotazioni possibili a questi 4 angoli
- * precisi
- * La D iniziale sta per "degrees"
-*/
-enum Rotation
+/* WORK IN PROGRESS */
+struct Tetrimino getTetrimino(TetriminoType type)
 {
-    D_0,
-    D_90,
-    D_180,
-    D_270
-};
+    Tetrimino t;
+    t.type = type;
+    switch (type)
+    {
+        case T_I: {
+            int values[1][4] = {
+                    1, 1, 1, 1
+            };
+            t.values = &values[0][0];
+            t.cols = 4;
+            t.rows = 1;
+            break;
+        }
+        case T_J: {
+            int values[2][3] = {
+                    2, 0, 0,
+                    2, 2, 2,
+            };
+
+            t.values = &values[0][0];
+            t.cols = 3;
+            t.rows = 2;
+            break;
+        }
+        case T_L: {
+            size_t i,j;
+            int values[2][3] = {
+                    0, 0, 3,
+                    3, 3, 3
+            };
+
+            t.values = &values[0][0];
+            t.cols = 3;
+            t.rows = 2;
+            break;
+        }
+        case T_S: {
+            int values[2][3] = {
+                    0, 5, 5,
+                    5, 5, 0,
+            };
+
+            t.values = &values[0][0];
+            t.cols = 3;
+            t.rows = 2;
+            break;
+        }
+        case T_Z: {
+            int values[2][3] = {
+                    7, 7, 0,
+                    0, 7, 7,
+            };
+
+            t.values = &values[0][0];
+            t.cols = 3;
+            t.rows = 2;
+            break;
+        }
+        case T_O: {
+            int values[2][2] = {
+                    4, 4,
+                    4, 4,
+            };
+
+            t.values = &values[0][0];
+            t.cols = 2;
+            t.rows = 2;
+            break;
+        }
+        case T_T: {
+            int values[2][3] = {
+                    0, 6, 0,
+                    6, 6, 6,
+            };
+
+            t.values = &values[0][0];
+            t.cols = 3;
+            t.rows = 2;
+            break;
+        }
+        default:
+            printf("Non so come hai fatto ma hai passato un valore non presente nell'enum");
+    }
+    return t;
+}
 
 /**
  * Struttura che associa ad ogni tetramino
